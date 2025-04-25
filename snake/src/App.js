1f0b5d0478f0429cbc2b12830639d493ceeb5601
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-25 11:03:38 +0530

    creating the grid

--- snake/src/App.js
+++ snake/src/App.js
@@ -1,3 +1,11 @@
 import React, {useState, useEffect, useRef} from "react";
 
+const gridSize = 20;
+const initialSnake = [{ x: 10, y: 10 }];
+const directions = {
+  ArrowUp: { x: 0, y: -1 },
+  ArrowDown: { x: 0, y: 1 },
+  ArrowLeft: { x: -1, y: 0 },
+  ArrowRight: { x: 1, y: 0 },
+};
 
