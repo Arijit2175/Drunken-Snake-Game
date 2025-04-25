436841fcce4945a4a1ccb9d890a5fa40c957cc84
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-25 11:16:50 +0530

    put everything in app.js

--- snake/src/App.js
+++ snake/src/App.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect, useRef} from "react";
 import "./App.css";
 
+function App(){
 const gridSize = 20;
 const initialSnake = [{ x: 10, y: 10 }];
 const directions = {
@@ -10,7 +11,6 @@ const directions = {
   ArrowRight: { x: 1, y: 0 },
 };
 
-export default function DrunkenSnake() {
   const [snake, setSnake] = useState(initialSnake);
   const [dir, setDir] = useState(directions.ArrowRight);
   const [food, setFood] = useState({ x: 5, y: 5 });
